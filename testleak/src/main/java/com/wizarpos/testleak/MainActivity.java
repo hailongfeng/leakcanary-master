6402cdcecfbaab0e8481383a5c674b8d1c219e8d
harlen <harlen_feng@163.com>
2016-04-13 15:44:06 +0800

    java工程测试。。。

--- testleak/src/main/java/com/wizarpos/testleak/MainActivity.java
+++ testleak/src/main/java/com/wizarpos/testleak/MainActivity.java
@@ -3,21 +3,8 @@ package com.wizarpos.testleak;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Debug;
-import android.os.Process;
 import android.os.SystemClock;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
-import com.squareup.leakcanary.RefWatcher;
-
-import java.io.File;
 
 public class MainActivity extends Activity {
     @Override
---
+++ z_java_test/src/main/java/com/example/MyClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.Calendar;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Calendar calendar=Calendar.getInstance();
+        System.out.println(calendar.getTimeInMillis());
+    }
+}
